8ba5b2fdc262ab26473d6d2f5dc92897f0733b73
AndrewKurano <andrew.kurano@triggerxr.com>
2025-07-29 01:37:46 -0700

    Updated nav bar

--- src/shared/navbar/Navbar.tsx
+++ src/shared/navbar/Navbar.tsx
@@ -42,7 +42,6 @@ export const Navbar: FC = () => {
                                 Resume
                             </Button>
                         </HStack>
-                        <ColorModeButton />
                     </Flex>
 
                     <MenuDrawer
